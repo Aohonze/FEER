a33e8418e305ffff28388e61a9e1a03cc6b37fa0
tyrone <1161763410@qq.com>
2021-03-25 23:45:56 +0800

    03.25样式更新

--- docs/.vuepress/config/sidebar-auto.js
+++ docs/.vuepress/config/sidebar-auto.js
@@ -7,9 +7,9 @@
  */
 module.exports = {
   
-  "/00.目录页/": [["01.个人博客.md","个人博客","/blog"],["02.面经整理.md","面经整理","/面经整理"],["03.读书笔记.md","读书笔记","/读书笔记"],["05.更多.md","更多","/更多"]],
+  "/00.目录页/": [["01.blog.md","blog","/blog"]],
   
   "/01.blog/": [{"title":"前端","collapsable":false,"children":[["01.前端/01.JS基础--继承.md","JS基础--继承","/pages/f11f7e/"],["01.前端/02.JS基础--拷贝.md","JS基础--拷贝","/pages/2738d1/"],["01.前端/03.JS创建对象模式.md","JS创建对象模式","/pages/855c50/"],["01.前端/10.Ajax跨域解决方案.md","Ajax跨域解决方案","/pages/9aeac8/"],["01.前端/20.EXPRESS中间件入门.md","EXPRESS中间件入门","/pages/dd05fd/"]]},{"title":"算法","collapsable":false,"children":[["20.算法/05.JS排序算法总结.md","JS排序算法总结","/pages/1b4491/"],["20.算法/30.剑指OFFER JavaScript版本解析.md","剑指OFFER JavaScript版本解析","/pages/d742a5/"],["20.算法/50.二叉树篇.md","二叉树篇","/pages/652ede0d7b81bbfb"]]},{"title":"笔记","collapsable":false,"children":[{"title":"JavaScript高级程序设计","collapsable":false,"children":[["30.笔记/01.JavaScript高级程序设计/03.第三章 JS 基础概念.md","第三章 JS 基础概念","/pages/f21a2a/"],["30.笔记/01.JavaScript高级程序设计/04.第四章 JS变量、作用域和内存问题.md","第四章 JS变量、作用域和内存问题","/pages/e49708/"],["30.笔记/01.JavaScript高级程序设计/05.第五章 引用类型.md","第五章 引用类型","/pages/6058b4/"],["30.笔记/01.JavaScript高级程序设计/06.第六章 面向对象的程序设计.md","第六章 面向对象的程序设计","/pages/7332b7/"],["30.笔记/01.JavaScript高级程序设计/07.第七章 函数表达式.md","第七章 函数表达式","/pages/4dc925/"]]}]},{"title":"面经","collapsable":false,"children":[["40.面经/01.HTML篇.md","HTML篇","/pages/a88295/"],["40.面经/02.CSS篇.md","CSS篇","/pages/2ab6a0/"],["40.面经/03.JS篇.md","JS篇","/pages/c52c25/"],["40.面经/04.浏览器篇.md","浏览器篇","/pages/e5c51c/"],["40.面经/05.网络篇.md","网络篇","/pages/439699/"],["40.面经/06.算法篇.md","算法篇","/pages/9a4ae7/"]]},{"title":"其他","collapsable":false,"children":[["60.其他/01.网站收藏.md","网站收藏","/pages/a49391/"],["60.其他/02.Markdown使用教程.md","Markdown使用教程",""],["60.其他/03.npm常用命令.md","npm常用命令",""],["60.其他/04.Git使用手册.md","Git使用手册",""],["60.其他/07.VSCODE插件指南.md","VSCODE插件指南","/pages/3802cc/"],["60.其他/10.npm packageJson属性详解.md","npm packageJson属性详解","/pages/dec4f3f00e71a312"],["60.其他/15.yaml语言教程.md","yaml语言教程","/pages/4e8444e2d534d14f"],["60.其他/50.计算机网络详解.md","计算机网络详解","/pages/1c11c0/"],["60.其他/60.学习备忘录.md","学习备忘录","/pages/a987b2/"]]}],
   
-  "catalogue": {"个人博客":"/blog","面经整理":"/面经整理","读书笔记":"/读书笔记","更多":"/更多"}
+  "catalogue": {"blog":"/blog"}
 }
